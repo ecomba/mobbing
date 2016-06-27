6d19d1a21eec4aa41123e114d6a90a9335456a22
Enrique Comba Riepenhausen <ecomba@8thlight.com>
2016-06-27 06:37:03 +0100

    Better help

--- lib/help.bash
+++ lib/help.bash
@@ -1,5 +1,39 @@
-help() {
-  echo "Usage: mob [options] <first pair username> <second pair username> <...>"
+_blank() {
   echo ""
-  echo "Options: NONE SO FAR"
+}
+
+print_help() {
+  echo "Usage: mob [options] or mob <first pair username> <second pair username> <...>"
+  _blank
+  echo "Options:"
+  _blank
+  echo "* -a           (mob -a <username> \"<full name>\")"
+  echo "               Adds a new author to the list of possible authors."
+  echo "               Example: mob -a ecomba \"Enrique Comba Riepenhausen\""
+  echo "* -h, --help   Prints this message"
+  echo "* -v           Prints the current version number"
+  _blank
+}
+
+print_author_added() {
+  _blank
+  echo "Added \"$1\" to the list of mobbing authors."
+  _blank
+}
+
+print_current_mob() {
+  _blank
+  if [[ ! -e "$CURRENT_MOB" ]] ; then
+    echo "You are not mobbing (or pairing) with anyone at the moment."
+  else
+    echo "The current mob is exported as follows:"
+    echo $(<$CURRENT_MOB)
+  fi
+  _blank
+}
+
+print_version() {
+  _blank
+  echo "Mobbing v$(mob_version)"
+  _blank
 }
